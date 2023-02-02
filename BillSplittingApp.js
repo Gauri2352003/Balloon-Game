93fab1f14fa37ebe65e95ed27322f6eb2dc764d3
Gauri2352003 <108190105+Gauri2352003@users.noreply.github.com>
2023-02-02 19:12:53 +0530

    Add files via upload

---
+++ BillSplittingApp.js
@@ -0,0 +1,8 @@
+function reuse(){
+    let a=document.getElementById("usd").value;
+    let b=document.getElementById("hii").value;
+    let result=a/b;
+    document.getElementById("hello").innerText="Result: ₹"+result;
+}
+
+
